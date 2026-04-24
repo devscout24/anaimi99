352866b905415474aebe780736085ad359495af1
Mdshahin12502077ms <md12502077ms@gmail.com>
2026-04-25 03:41:03 +0600

    a

--- routes/api.php
+++ routes/api.php
@@ -6,10 +6,6 @@ use Illuminate\Support\Facades\Route;
 
 require __DIR__ . '/api_farhad.php';
 
-<<<<<<< HEAD
-require __DIR__ . '/api_abdullah.php';
-=======
 require __DIR__ . '/Shahin.php';
 
 require __DIR__ . '/abdullah.php';
->>>>>>> 504c85536fe547ec3b62f6a36011de058c8cf947
